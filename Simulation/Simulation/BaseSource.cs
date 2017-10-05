856561428ea8171defb7463c98757dec2c488331
callidex <callidex@users.noreply.github.com>
2017-10-05 17:11:41 +1000

    broken clock is right twice a day

--- Simulation/Simulation/BaseSource.cs
+++ Simulation/Simulation/BaseSource.cs
@@ -8,13 +8,8 @@
 
         public virtual long GetCount(double  dt)
         {
-          // having a moment...
-            // counts in 1 sec = CurrentFreq
-
-            // how much time is dt
-
-            // (dt/SIMULATION_CLOCK_MS) * 1000 = number of seconds
-            return (long) (CurrentFreq * (dt/Program.SIMULATION_CLOCK_MS)) ;
+      
+            return (long) ((long) (CurrentFreq/1000) * dt) ;
         }
     }
 }
