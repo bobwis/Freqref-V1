829de42b8b9b6541975d52ef0b99dec337ddd67b
callidex <callidex@users.noreply.github.com>
2017-10-09 12:08:24 +1000

    tweak with some explanation

--- Freqref/genpid.c
+++ Freqref/genpid.c
@@ -37,27 +37,23 @@
  static float derivative;
  static float lasterr;
 
- static uint64_t baseunit;            // =???
+ static uint64_t baseunit =1000;           // units in a second (in the sim thats the CLOCK), here if the intervals are in ms, it's 1000. 
 
+ // sp and pv are the raw counts, in that interval, no need to touch them outside of this method
  float compute(uint64_t interval, float pv, float sp)
  {
-	
+	float ratio = (2 * baseunit) / interval;	
 	// is the interval in ms? This needs to be lined up with what pvMin/pvMax is.
-
-
-	float ratio = (2 * baseunit) / interval;
 	sp *= ratio;
 	pv *= ratio;
 
+	// everything below is bog standard PID, nothing to tweak
 	float err;
 	pv = clamp(pv,pvMin,pvMax);
 	pv = scalevalue(pv, pvMin, pvMax, -1.0, 1.0);
-
-	//scale the setpoint
 	sp = clamp(sp, pvMin, pvMax);
 	sp = scalevalue(sp, pvMin, pvMax, -1.0, 1.0);
-	err = sp - pv;
-		    
+	err = sp - pv;	    
 	integral = integral + (err  *interval  );
 	derivative = (err - lasterr)  /interval;
 	float output = (kp * err) + (ki* integral) + (kd * derivative);
